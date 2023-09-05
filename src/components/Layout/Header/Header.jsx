96efd17864430a24828a2de0d2052db245a52a8d
Jasim Uddin <jasim.ice.iu@gmail.com>
2023-09-05 12:35:01 +0200

    some components added

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,13 +1,14 @@
 import React, { Fragment } from 'react';
 import './Header.css';
 import meals from '../../../assets/meals.jpg';
+import HeaderCartButton from '../HeaderCartButton/HeaderCartButton';
 
 const Header = () => {
     return (
         <Fragment>
             <header>
                 <h1>FoodieFare</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div>
                 <img src={meals} alt="A table full of delicious food!" />
